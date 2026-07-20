e53aadc36dea174fae881ebdd566dca2250f1abc
Kalkoenman <kalkoenman@users.noreply.github.com>
2026-07-20 15:14:42 +0200

    Fix chrono dependency version workspace conflict by bumping version for sensor-link-firmware and update use of deprecated function.

--- sensor-link-firmware/src/utils/time.rs
+++ sensor-link-firmware/src/utils/time.rs
@@ -1,15 +1,19 @@
-use chrono::NaiveDateTime;
+use chrono::{DateTime, NaiveDateTime};
 
 #[inline]
 pub fn datetime_from_millis(millis: i64) -> Result<NaiveDateTime, ()> {
     let ts_secs = millis / 1000;
     let ts_ns = ((millis % 1000) as u32) * 1_000_000;
-    NaiveDateTime::from_timestamp_opt(ts_secs, ts_ns).ok_or(())
+    DateTime::from_timestamp(ts_secs, ts_ns)
+        .map(|dt| dt.naive_utc())
+        .ok_or(())
 }
 
 #[inline]
 pub fn datetime_from_micros(micros: i64) -> Result<NaiveDateTime, ()> {
     let ts_secs = micros / 1000000;
     let ts_ns = ((micros % 1000000) as u32) * 1_000;
-    NaiveDateTime::from_timestamp_opt(ts_secs, ts_ns).ok_or(())
+    DateTime::from_timestamp(ts_secs, ts_ns)
+        .map(|dt| dt.naive_utc())
+        .ok_or(())
 }

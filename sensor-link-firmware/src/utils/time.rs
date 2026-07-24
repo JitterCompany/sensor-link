cef4869c9d1d9d33ab77fc640d89cd4ca78c75ae
Kalkoenman <kalkoenman@users.noreply.github.com>
2026-07-24 16:47:24 +0200

    Add custom error type for getting date-time from timestamp.

--- sensor-link-firmware/src/utils/time.rs
+++ sensor-link-firmware/src/utils/time.rs
@@ -1,19 +1,23 @@
 use chrono::{DateTime, NaiveDateTime};
 
+pub enum TimeError {
+    InvalidTimestamp(i64),
+}
+
 #[inline]
-pub fn datetime_from_millis(millis: i64) -> Result<NaiveDateTime, ()> {
+pub fn datetime_from_millis(millis: i64) -> Result<NaiveDateTime, TimeError> {
     let ts_secs = millis / 1000;
     let ts_ns = ((millis % 1000) as u32) * 1_000_000;
     DateTime::from_timestamp(ts_secs, ts_ns)
         .map(|dt| dt.naive_utc())
-        .ok_or(())
+        .ok_or(TimeError::InvalidTimestamp(millis))
 }
 
 #[inline]
-pub fn datetime_from_micros(micros: i64) -> Result<NaiveDateTime, ()> {
+pub fn datetime_from_micros(micros: i64) -> Result<NaiveDateTime, TimeError> {
     let ts_secs = micros / 1000000;
     let ts_ns = ((micros % 1000000) as u32) * 1_000;
     DateTime::from_timestamp(ts_secs, ts_ns)
         .map(|dt| dt.naive_utc())
-        .ok_or(())
+        .ok_or(TimeError::InvalidTimestamp(micros))
 }
